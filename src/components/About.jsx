4602fbc15b2743b65e1cfb5e309f5f47284b95d6
RAMYA <kandagatlaramya5@gmail.com>
2024-12-15 00:49:44 +0530

    initial commit: portfolio structure changes added

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">
+          Highly skilled Software Engineer with almost 3 years of experience in
+          frontend development, specializing in React.js. Proficient in data
+          structures and algorithms, with a proven track record of successfully
+          delivering features across multiple platforms and ensuring stable
+          releases. Experienced in developing responsive websites and committed
+          to excellence in problem-solving and communication. Possesses strong
+          knowledge of React.js, JavaScript, and Webpack.
+        </p>
+        <br />
+        <p className="text-xl">
+          Highly skilled Software Engineer with almost 3 years of experience in
+          frontend development, specializing in React.js. Proficient in data
+          structures and algorithms, with a proven track record of successfully
+          delivering features across multiple platforms and ensuring stable
+          releases. Experienced in developing responsive websites and committed
+          to excellence in problem-solving and communication. Possesses strong
+          knowledge of React.js, JavaScript, and Webpack.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
